95596ead9e049cbd03c34fced6588bc8f4d8552c
gmm15 <gmm15@duke.edu>
2015-04-12 18:11:07 -0400

    matt's code

---
+++ src/Main.py
@@ -0,0 +1,17 @@
+'''
+Created on Apr 11, 2015
+
+@author: gmccabe
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
