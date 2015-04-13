69669e6d528132dea79d06c84c8bafe23bd2b6bd
janvicshah <shah.janvi@gmail.com>
2015-04-13 14:07:24 -0400

    query() now has a limit parameter and returns the JSON dictionary directly

--- src/single_drug_all.py
+++ src/single_drug_all.py
@@ -14,20 +14,33 @@ import sys
 
 #---------------------------------------------------------#
 
-# generate query url using given input parameters
-def query(drug_name):
+# generate JSON dictionary using given input parameters
+def query(drug_name, limit):
     prefix = "https://api.fda.gov/drug/event.json?api_key=vZC1Gh1XyJl58wZKsMfJifZisDOFRsGBCij3G32v&search=patient.drug.openfda.brand_name:"
     q = "%22"
-    return prefix + q + drug_name + q + "w"
+    
+    URL = prefix + q + drug_name + q + '&limit=' + str(limit)
+    j = urllib2.urlopen(URL)
+    d = json.load(j)
+    return d
 
 #---------------------------------------------------------#
 
 # store command line argument (string for drug name)
 drug_name = sys.argv[1]
-# load query result in json format
-URL = query(drug_name)
-j = urllib2.urlopen(URL)
-data = json.load(j)
+
+limit = 1
+init = query(drug_name, limit)
+
+# total # of records for that drug
+total = init['meta']['results']['total'] 
+
+
+
+
+
+# need to set the limit on the query 
+
 # process data
 '''
 analyze data here
