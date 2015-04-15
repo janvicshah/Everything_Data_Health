fc79e017ba129124e546408b31e686de974aba24
gmm15 <gmm15@duke.edu>
2015-04-15 10:49:05 -0400

    single-drug-all update convert-age-method

--- src/single_drug_all.py
+++ src/single_drug_all.py
@@ -14,39 +14,39 @@ import sys
 
 #---------------------------------------------------------#
 
-<<<<<<< HEAD
+
 # generate query url using given input parameters
+#def query(drug_name):
+
+# generate JSON dictionary using given input parameters
 def query(drug_name):
-   
-=======
+    
 # generate JSON dictionary using given input parameters
 def query(drug_name, limit):
->>>>>>> origin/master
-    prefix = "https://api.fda.gov/drug/event.json?api_key=vZC1Gh1XyJl58wZKsMfJifZisDOFRsGBCij3G32v&search=patient.drug.openfda.brand_name:"
-    q = "%22"
+
+     prefix = "https://api.fda.gov/drug/event.json?api_key=vZC1Gh1XyJl58wZKsMfJifZisDOFRsGBCij3G32v&search=patient.drug.openfda.brand_name:"
+     q = "%22"
+    return prefix + q + drug_name + q + "w"
     
     URL = prefix + q + drug_name + q + '&limit=' + str(limit)
     print URL
     j = urllib2.urlopen(URL)
     d = json.load(j)
     return d
+ 
+ #---------------------------------------------------------#
+ 
 
-#---------------------------------------------------------#
+ # store command line argument (string for drug name)
+ #drug_name = 'Lipitor'
+ #sys.argv[1] = 'Lipitor'
+@@ -32,6 +43,25 @@ def query(drug_name):
+ # j = urllib2.urlopen(URL)
+ # data = json.load(j)
+ # print(data)
 
-<<<<<<< HEAD
-# store command line argument (string for drug name)
-#drug_name = 'Lipitor'
-#sys.argv[1] = 'Lipitor'
-# drug_name = raw_input('enter a drug name :')
-# # load query result in json format
-# URL = query(drug_name)
-# j = urllib2.urlopen(URL)
-# data = json.load(j)
-# print(data)
-=======
 # query user for the input (string for drug name)
 drug_name = raw_input("Drug name?: ")
-
 limit = 1
 init = query(drug_name, limit)
 
@@ -55,14 +55,12 @@ total = init['meta']['results']['total']
 
 # find the # of records associated with the age, weight, sex
 sex = raw_input("Sex (Input : ")
-
 # need to set the limit on the query = total # of records
 # can only query 100 records at a time, so need to divide by 100 and query that many times
 data = query(drug_name, total)
 print len(data['results'])
 
->>>>>>> origin/master
-# process data
-'''
+
+ # process data
+ '''
 analyze data here
-'''
